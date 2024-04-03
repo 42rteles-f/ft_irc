c35db036561016c82338f5e3c1fe1109919a9a2b
Rubens Teles Freire Junior <rteles-f@c2r6s2.42lisboa.com>
2024-04-03 01:56:26 +0100

    implemented the conncetion class, that encapsulates the vectors for clients and strucutures

--- headers/Client.hpp
+++ headers/Client.hpp
@@ -6,7 +6,7 @@
 /*   By: rteles-f <rteles-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 17:30:16 by rteles-f          #+#    #+#             */
-/*   Updated: 2024/04/03 01:41:53 by rteles-f         ###   ########.fr       */
+/*   Updated: 2024/04/03 01:54:50 by rteles-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,6 @@ class Client {
 
 		Client& operator=(const Client& tocopy);
 
-		bool	hasInput(void);
 		bool	update(void);
 		bool	isClosed(void);
 
--- src/server/Client.cpp
+++ src/server/Client.cpp
@@ -6,7 +6,7 @@
 /*   By: rteles-f <rteles-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 21:37:16 by rteles-f          #+#    #+#             */
-/*   Updated: 2024/04/03 01:40:59 by rteles-f         ###   ########.fr       */
+/*   Updated: 2024/04/03 01:54:43 by rteles-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,10 +31,6 @@ Client& Client::operator=(const Client& tocopy) {
 	this->input = tocopy.input;
 	this->_command = tocopy._command;
 	return (*this);
-}
-
-bool	Client::hasInput(void) {
-	return (socket.revents & POLLIN ? true : false);
 }
 
 bool	Client::isClosed(void) {
